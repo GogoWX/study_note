951a6bc56dbcb67025d839a513250aa056c8a67e
Wxw <wangxu5277@163.com>
2020-07-08 15:34:49 +0800

    up demo_sse code

--- learnNote/html_css_js_learn/php/demo_sse.php
+++ learnNote/html_css_js_learn/php/demo_sse.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: text/event-stream');
+header('Content-Type: application / x-httpd-php');
 header('Catch-control: no-catch');
 $time = date('r');
 echo 'data: The server time is: {$time}nn';
