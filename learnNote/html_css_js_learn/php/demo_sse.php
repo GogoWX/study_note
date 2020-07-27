ad2d08915633fea988c60df4ac8a4faf4f2bd871
Wxw <wangxu5277@163.com>
2020-07-27 16:25:10 +0800

    updata

--- learnNote/html_css_js_learn/php/demo_sse.php
+++ learnNote/html_css_js_learn/php/demo_sse.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-Type: text/event-stream');
 header('Catch-control: no-catch');
+header('Connection: keep-alive');
 $time = date('r');
-echo "data: The server time is: {$time}nn";
+echo "data: The server time is: {$time}\n\n";
 flush();
 ?>
